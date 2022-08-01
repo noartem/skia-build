b4f21d7f4241cd470ffe5b291ab58aa27d06bee1
Noskov Artem <nowasmawesome@gmail.com>
2022-08-01 19:05:20 +0500

    Fix linux build

--- script/prepare_linux.sh
+++ script/prepare_linux.sh
@@ -8,7 +8,8 @@ apt-get update -y
 apt-get install build-essential software-properties-common -y
 apt-get update
 apt-get install gcc-9 g++-9 -y
-apt-get install clang-11 -y
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+update-alternatives --config gcc 
 
 apt-get install git python wget -y
 apt-get install ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip -y
